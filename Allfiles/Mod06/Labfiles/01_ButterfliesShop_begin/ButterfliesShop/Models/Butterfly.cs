9cbb399ffe3250b1d36a44f38e9284a5c5a6ed46
Hiago Grades <hiagogrades@hotmail.com>
2022-03-30 22:18:56 -0300

    Inicia Mod06 e README.md

---
+++ Allfiles/Mod06/Labfiles/01_ButterfliesShop_begin/ButterfliesShop/Models/Butterfly.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http; //ToDo: 1- Adicionar a biblicoteca HTTP
+using System.ComponentModel.DataAnnotations; //ToDo: 2 - trabalhando com data Annotation
+
+namespace ButterfliesShop.Models
+{
+    public class Butterfly
+    {
+        //ToDo: 2 - Adicionando os métodos
+        public int Id { get; set; }
+
+        [Display(Name = "Common Name:")]
+        public string CommonName { get; set; }
+
+        [Display(Name = "Butterfly Family:")]
+        public Family? ButterflyFamily { get; set; }
+
+        [Display(Name = "Butterfly Quantity:")]
+        public int? Quantity {get; set; }
+
+        [Display(Name = "Characteristics:")]
+        public string Characteristics { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Updated on:")]
+        [DisplayFormat(DataFormatString ="{0:dd/MM/yy}")]
+        public DateTime CreatedDate { get; set; }
+
+        [Display(Name = "Butterflies Picture:")]
+        public IFormFile PhotoAvatar { get; set; }
+        public string ImageName { get; set; }
+        public byte[] PhotoFile { get; set; }
+        public string ImageMimeType { get; set; }
+    }
+}
