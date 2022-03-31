6b2493d2afeb7361693cb0efce1710e89e8b2fd1
Hiago Grades <hiagogrades@hotmail.com>
2022-03-30 22:24:16 -0300

    Inicia Mod01

---
+++ Allfiles/Mod01/Labfiles/01_ActorsRazorPages_begin/ActorsRazorPages/ActorsRazorPages/Models/Actor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActorsRazorPages.Models
+{
+    public class Actor
+    {
+        public int Id { get; set; }
+        public string FirtsName { get; set; }
+        public string LastName { get; set; }
+        public string Konownfor { get; set; }
+        public bool OscarWinner { get; set; }
+        public string ImageName { get; set; }
+
+
+    }
+}
---
+++ Allfiles/Mod01/Labfiles/01_ActorsRazorPages_begin/ActorsRazorPages/ActorsRazorPages/Models/IData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActorsRazorPages.Models
+{
+    public interface IData
+    {
+
+        //TODO - Tarefa 3, exercício 12
+        List<Actor> ActorsList { get; set; }
+        List<Actor> ActorsInitializeData();
+        Actor GetActorById(int? id);
+    }
+}
